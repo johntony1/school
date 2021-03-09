72a054a14901c9bf7e6e81d1dbeffd1ff59dd65a
johntony1 <80322463+johntony1@users.noreply.github.com>
2021-03-09 11:20:25 +0100

    Add files via upload

---
+++ ENG1804790.m
@@ -0,0 +1,32 @@
+%NAME :         OREVBA JOHN TONY
+%MAT NO:        ENG1804790
+%DEPARTMENT:    COMPUTER ENGINEERING
+%COURSE CODE:   ECP281
+
+% QUESTION 1
+a = 1;
+b = 1;
+c = 1;
+x = 1.6;
+
+d = sqrt(b^2 - 4*a*c);
+
+x1 = (-b + d)/(2*a);
+x2 = (-b - d)/(2*a);
+
+
+% Question 1a
+disp( a*(x^2) + b*x + c );
+
+% Question 1b
+disp([x1 x2]);
+
+
+
+% QUESTION 2
+A = [1 2 3;1 5 0; 0.5 1.5 4];
+B = [6;6;6];
+disp(A\B);
+
+
+
